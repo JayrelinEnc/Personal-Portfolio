6a1edf2fbce8f465753e682f861e56a8e8ef75ac
Jayrelin Encarnacion <462607@Device-01-R307.local>
2026-06-10 12:48:50 -0400

    Capstone-06-10-2026

--- script.js
+++ script.js
@@ -1,34 +1,45 @@
-// select my elements
-let homeContent = document.getElementById("home-content");
+ let homeContent = document.getElementById("home-content");
 let skills = document.getElementById("skills");
 let education = document.getElementById("education");
 let galleryWalk = document.getElementById("gallery");
-let contact = document.getElementById("contact");
+ let contact = document.getElementById("contact");
+ function indicator() {
+    const currentBookmark = window.location.hash;
 
+    // Reset all colors
+    homeContent.style.color = "";
+    skills.style.color = "";
+    education.style.color = "";
+    galleryWalk.style.color = "";
+    contact.style.color = "";
 
+    switch (currentBookmark) {
+        case "":
+            homeContent.style.color = "#b74b4b";
+            break;
 
+        case "#Skills":
+            skills.style.color = "#b74b4b";
+            break;
 
-//functions
-function selectHome(){
-    homeContent.style.color = "#b74b4b"
-}
-function selectSkills(){
-    skills.style.color = "#b74b4b"
-}
-function selectEducation(){
-    education.style.color = "#b74b4b"
-}
-function selectGallery(){
-    galleryWalk.style.color = "#b74b4b"
-}
-function selectContact(){
-    contact.style.color = "#b74b4b"
-}
-    //eventListeners
+        case "#Education":
+            education.style.color = "#b74b4b";
+            break;
 
-    homeContent.addEventListener("click", selectHome);
-    skills.addEventListener("click", selectSkills);
-    education.addEventListener("click", selectEducation);
-    galleryWalk.addEventListener("click", selectGallery);
-    contact.addEventListener("click", selectContact);
+        case "#galleryWalk":
+            galleryWalk.style.color = "#b74b4b";
+            break;
+
+        case "#Contact":
+            contact.style.color = "#b74b4b";
+            break;
+
+        default:
+            homeContent.style.color = "#b74b4b";
+    }
+
+ }
+window.addEventListener("DOMContentLoaded", indicator);
+window.addEventListener("hashchange", indicator);
+
 
